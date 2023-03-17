f2ecefdb5034c9f1342daa77598d3f72f524d64c
camille-clappaz <camille.clappaz@laplateforme.io>
2023-03-17 16:16:58 +0100

    creation de deux tableaux de 3 cartes

--- memory/memory.php
+++ memory/memory.php
@@ -4,47 +4,51 @@ $state = "false";
 $id_card = 1;
 $img_face_down = "urldown";
 $img_face_up = "urlup";
-$card = new Card($id_card, $img_face_down, $img_face_up, $state);
-// if (isset($_POST["retourner"])) { //FONCTIONNE
-//     $state = "false";
-//     $id_card = 1;
-//     $img_face_down = "urldown";
-//     $img_face_up = "urlup";
 
-//     if ($_POST["retourner"] == "false") {// avec un seul = ça ne fonctionne pas ATTENTION!!!!
-//         $state='true';
-//         echo $img_face_up;
-//     } else {
-//         $state="false";
-//         echo $img_face_down;
-//     }
-// }
-// else{
-//     echo $img_face_down;
-// }
-
-if (isset($_POST["retourner"])) {
-    if ($_POST["retourner"] == "false") { // avec un seul = ça ne fonctionne pas ATTENTION!!!!
-        $state = "true";
-        $card->setState($state);
-        $card->state();
-    } else {
-        $state = "false";
-        $card->setState($state);
-        $card->state();
-    }
-} else {
-    $state = "false";
-    $card->setState($state);
-    $card->state();
+for ($i = 0; $i < 3; $i++) {
+    $card[$i] = new Card($id_card, $img_face_down, $img_face_up, $state);
+    $card[$i]->setId_card($i);
+    $card[$i]->setImg_face_up($i . "up");
+    $card[$i]->setState("false");
+    $array1= array($card);
 }
+for ($i = 0; $i < 3; $i++) {
+    $card[$i] = new Card($id_card, $img_face_down, $img_face_up, $state);
+    $card[$i]->setId_card($i);
+    $card[$i]->setImg_face_up($i . "up");
+    $card[$i]->setState("false");
+    $array2= array($card);
+}
+$array = array_merge($array1, $array2);
+
+var_dump($card[1]);
+
+
+
+// if (isset($_POST["retourner"])) {
+//     if ($_POST["retourner"] == "false") { // avec un seul = ça ne fonctionne pas ATTENTION!!!!
+//         $state = "true";
+//         $card->setState($state); // important pour mettre a jour le $state
+//         $card->state();
+//     } else {
+//         $state = "false";
+//         $card->setState($state);
+//         $card->state();
+//     }
+// } else {
+//     $state = "false";
+//     $card->setState($state);
+//     $card->state();
+// }
 
 
 
 
-var_dump($card);
+
+// var_dump($array, $array1, $array2);
 // var_dump($_GET);
 ?>
 <form action="" method="post">
     <button type="submit" name="retourner" value=<?= $state ?>>Retourner</button>
 </form>
+
